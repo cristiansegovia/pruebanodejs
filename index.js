bf87fe1ab756a9de4d5485484e3da553e876fe46
cristiansegovia <cristianxsegovia@gmail.com>
2022-08-23 17:42:06 -0300

    cambios de index.js 2

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-console.clear();
-import { createServer } from "http";
+import { createServer } from "http"
 
-console.log("ingresando a app");
-const PORT= process.env.PORT || 3300;
+console.log("ingresando a app")
+const PORT= process.env.PORT || 3300
 const server=createServer((req,res)=>{
-	console.log("peticion recibida");
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-  	res.write('Hello World!');
-  	res.end();
-});
+	console.log("peticion recibida")
+	res.writeHead(200, {'Content-Type': 'text/plain'})
+  	res.write('Hello World!')
+  	res.end()
+})
 
 server.listen(PORT, function (){
 	console.log("servidor escuchando puerto ", PORT)
-});
+})
