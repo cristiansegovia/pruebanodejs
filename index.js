40594baf5dde4f52a811a6ebc695c1722b3ea152
cristiansegovia <cristianxsegovia@gmail.com>
2022-08-23 17:38:01 -0300

    cambios de index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.clear();
 import { createServer } from "http";
 
 console.log("ingresando a app");
-const PORT= process.env.PORT || 3300
+const PORT= process.env.PORT || 3300;
 const server=createServer((req,res)=>{
 	console.log("peticion recibida");
 	res.writeHead(200, {'Content-Type': 'text/plain'});
