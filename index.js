64cf4f026552d872d2b098a0a26d73cd77ca67ba
cristiansegovia <cristianxsegovia@gmail.com>
2022-08-23 17:18:58 -0300

    cambios de puerto y start

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 console.clear();
 import { createServer } from "http";
 
+console.log("ingresando a app");
+const PORT= process.env.PORT || 3300
 const server=createServer((req,res)=>{
 	console.log("peticion recibida");
 	res.end("recibida colega");
 });
 
-server.listen(3300);
+server.listen(PORT, function (){
+	console.log("servidor escuchando puerto ", PORT)
+});
