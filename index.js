b29eeb87d908fbc24b34a8048b22ea682a6c63fb
cristiansegovia <cristianxsegovia@gmail.com>
2022-08-23 17:31:20 -0300

    cambios de index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ console.log("ingresando a app");
 const PORT= process.env.PORT || 3300
 const server=createServer((req,res)=>{
 	console.log("peticion recibida");
-	res.send("recibida colega");
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+  	res.write('Hello World!');
+  	res.end();
 });
 
 server.listen(PORT, function (){
